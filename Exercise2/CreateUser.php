dc4d7e1c129e5cfb49ef7a2b461e8a7f984fd8e8
nboonyasitin <nboonyasitin@gmail.com>
2020-12-05 22:56:49 -0600

    Updated HTML and PHP

--- Exercise2/CreateUser.php
+++ Exercise2/CreateUser.php
@@ -1,17 +1,30 @@
 <?php
-$mysqli = new mysqli("database_URL", "nahathboonyasit", "umee4coh",
+$mysqli = new mysqli("mysql.eecs.ku.edu", "nahathboonyasit", "umee4coh",
 "nahathboonyasit");
+
 /* check connection */
 if ($mysqli->connect_errno) {
  printf("Connect failed: %s\n", $mysqli->connect_error);
  exit();
 }
-$query = "SELECT Name, CountryCode FROM City ORDER by ID DESC LIMIT 50,5";
+
+$username = $_POST ["username"];
+// if($result = mysql_query($query))
+// {
+//     trigger_error('Username already in use.', E_USER_WARNING);
+// }
+// else
+// {
+//     if($_POST["username"] != '' && $num_rows < 1) {
+//         $query = "INSERT INTO Users (user_id) VALUES (\"$username\");";
+// }
+
 if ($result = $mysqli->query($query)) {
  /* fetch associative array */
  while ($row = $result->fetch_assoc()) {
- printf ("%s (%s)\n", $row["Name"], $row["CountryCode"]);
+ printf ("%s (%s)\n", $row["Username"], $row["Password"]);
  }
+
  /* free result set */
  $result->free();
 }

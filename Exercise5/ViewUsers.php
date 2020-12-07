7123b0fc6759321291ef536d672a6916435f99d8
nboonyasitin <nboonyasitin@gmail.com>
2020-12-06 21:29:06 -0600

    ex 5

--- Exercise5/ViewUsers.php
+++ Exercise5/ViewUsers.php
@@ -1,3 +1,26 @@
 <?php 
-printf("image of user and posts on SQL table attached to the file.");
+$mysqli = new mysqli("mysql.eecs.ku.edu", "nahathboonyasit", "umee4coh",
+"nahathboonyasit");
+
+/* check connection */
+if ($mysqli->connect_errno) 
+{
+    printf("Connect failed: %s\n", $mysqli->connect_error);
+    exit();
+}
+
+$query = "SELECT user_id FROM Users;";
+   
+$result = $mysqli->query($query);
+   while ($row = $result->fetch_assoc())
+   {
+       echo "" . $row["user_id"] . "<br>";
+   }
+printf("image of user and posts on SQL table also attached to the file.");
+
+$result->free();
+ /* free result set */
+ 
+/* close connection */
+$mysqli->close();
 ?> 

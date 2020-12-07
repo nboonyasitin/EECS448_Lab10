63ae7ad2dcae62bed1e49424e65b5bfd5404a342
nboonyasitin <nboonyasitin@gmail.com>
2020-12-06 21:15:23 -0600

    ex4

--- Exercise5/ViewUsers.php
+++ Exercise5/ViewUsers.php
@@ -1,5 +1,3 @@
 <?php 
-header('content-type: image/png'); 
-$img = "ex4.png";//the real image url. 
-echo file_get_contents($img); 
+printf("image of user and posts on SQL table attached to the file.");
 ?> 
